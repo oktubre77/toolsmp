6050d53efdf259191c322d8f5c66e6b8d2790c2b
German Mendoza <german.mendoza72@gmail.com>
2021-06-15 23:01:52 -0300

    con template EJS

--- main.js
+++ main.js
@@ -1,15 +1,137 @@
+var express = require('express');
+var app = express();
 var axios = require('axios');
+const { response } = require('express');
 require('dotenv').config();
+//middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('static'));
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
+//Definir puerto
+app.set('port', process.env.PORT || 5001);
+
+
+//Define Variable Global Token
+const access_token = process.env.ACCES_TOKEN;
+console.log(access_token)
+console.log(process.env.PORT)
+//Ruta estatica 
+app.use(express.static(__dirname + "/public"))
+
+//Ruta Raiz
+app.get('/', function(req, res){
+
+	res.render("index" , {titulo : "mi titulo dinamico"});
+			
+});
+
+app.post('/procesar', function(req, res){
+	var id = req.body.codigo;
+	console.log(id);
+	
+		
+	var config = {
+		method: 'get',
+		url: 'https://api.mercadopago.com/v1/payments/'+id+'?access_token='+access_token
+		,headers: { }
+	  };
+	  
+	  axios(config)
+	  .then(function (response) {
+		console.log(JSON.stringify(response.data));
+		console.log (response.data.date_created.substring(0,10))
+		
+		res.render("procesarid" , {
+			respuesta : "Resultado Api MercadoPago",
+			FechaCreacion : `FechaCreacion : ${response.data.date_created.substring(0,10)}`,
+			ID_Operacion : `ID_Operacion : ${response.data.id}`,
+			Codigo_Autorizacion : `Codigo_Autorizacion : ${response.data.authorization_code}`,
+			Status : `Status : ${response.data.status}`,
+			Status_detalle : `Status_detalle : ${response.data.status_detail}`,
+			Monto : `Monto : ${response.data.transaction_amount}`,
+			Cod_Comercios : `Cod_Comercios : ${response.data.merchant_number}`,
+			Primeros6_Digitos : `Primeros6_Digitos : ${response.data.card.first_six_digits}`,
+			Ultimo4_Digitos : `Ultimo4_Digitos : ${response.data.card.last_four_digits}`
+			
+		  });
+	  })
+	  .catch(function (error) {
+		console.log(error);
+	  });
+	  
+});
+
+app.post('/procesartodo', function(req, res){
+	let id1 = req.body.codigos;
+	id1.split(" ").join(" ");
+	console.log("primero ."+id1);
+	let id = id1.split(",");
+	console.log("Ultimo .."+id);
+	console.log("Largo de ID .."+id.length);
+
+	var tagline = "No programming concept";
+
+	for (let i = 0; i < id.length; i++) {	
+		
+	let config = {
+		method: 'get',
+		url: 'https://api.mercadopago.com/v1/payments/'+id[i]+'?access_token='+access_token
+		,headers: { }
+	  };
+	  
+	
+			axios(config)
+			.then(function (response) {
+				
+				
+				res.render("procesatodo" , {
+					respuesta : response.data,
+					tagline: tagline					
+				});
+			})
+	     
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+});
+
+
+app.use((req ,res , next) =>{
+	res.status(404).render("404",{
+		titulo : "404",
+		Descripcion : "Pagina no Encontrada"
+	})
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express se ha iniciado en http://localhost:' +
+    app.get('port') + '; presione Ctrl-C para cerrar el servidor.' );
+}
+);
+
+/*
 function getMP_ID()
 {
 	
-	mensaje.innerHTML=' ';
-	console.log('Limpia Div'+mensaje.innerHTML);
+	//const access_token = process.env.ACCES_TOKEN;
+	//mensaje.innerHTML=' ';
+	//console.log('Limpia Div'+mensaje.innerHTML);
 	var id = document.getElementById("cod").value;
-	console.log(id);
+	//console.log(id);
+
+		var loading = document.getElementById('loading');
+		var mensaje = document.getElementById('mensaje');
+		console.log(access_token);
+
+		var boton = document.getElementById('carga_ajax');
+		boton.addEventListener('click', function() {
+		loading.style.display = 'block';
 	
-	var access_token = process.env.NODE_ENV || 'acces_token';
 	axios.get('https://api.mercadopago.com/v1/payments/'+id+'?access_token='+access_token, {
 		responseType: 'json'
 	  })
@@ -27,7 +149,8 @@ function getMP_ID()
 	  })
 	  .then(function () {
 	  });
-}
+} 
+		)}
 
 function getMP_AllID()
 {
@@ -40,7 +163,7 @@ function getMP_AllID()
 	var idsToCancel = datosToSplit.split(",");
 	console.log('idsToCancel : '+idsToCancel);
 	
-	var access_token = process.env.NODE_ENV || 'acces_token';
+	//var access_token = process.env.ACCES_TOKEN || 'acces_token';
 	for (var i = 0; i < idsToCancel.length; i++) {
 			axios.get('https://api.mercadopago.com/v1/payments/'+idsToCancel[i]+'?access_token='+access_token, {
 				responseType: 'json'
@@ -67,7 +190,7 @@ function getByFechaMP()
 	
 	mensaje.innerHTML=' ';
 	console.log('Limpia Div'+mensaje.innerHTML);
-	var access_token = process.env.NODE_ENV || 'acces_token';
+	//var access_token = process.env.NODE_ENV || 'acces_token';
 	var FechaIni = document.getElementById("start").value;
 	var fechaInicio = FechaIni+'T00:00:00.001-04:00';
 	console.log(fechaInicio)
@@ -156,4 +279,4 @@ if (typeof window !== 'undefined') {
     window.myputCancelMP = function() {
         return putCancelMP();
     }
-}
+}*/
